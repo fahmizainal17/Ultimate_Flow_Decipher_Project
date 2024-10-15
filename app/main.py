986e63a71b38342cabfce38fa0c757d11c7a830c
fahmizainal17 <fahmizainal9@gmail.com>
2024-10-15 14:33:30 +0800

    fix main

--- app/main.py
+++ app/main.py
@@ -7,31 +7,29 @@ from module.data_cleaner_module.columns_definer import Questionnaire_Definer
 from module.data_cleaner_module.rows_definer import Keypress_Decoder
 from module.data_cleaner_module.dataframe_decoder import Questionnaire_Keypress_Decoder
 
-if check_password():
-    page_style()
-    # Ensure to initialize the class
-    ivr_cleaner = IVR_Data_Cleaner()
 
-    # Ensure to initialize the class
-    questionnaire_definer = Questionnaire_Definer()
 
-    keypress_decoder = Keypress_Decoder()
+page_style()
+# Ensure to initialize the class
+ivr_cleaner = IVR_Data_Cleaner()
 
-    questionnaire_keypress_decoder = Questionnaire_Keypress_Decoder()
+# Ensure to initialize the class
+questionnaire_definer = Questionnaire_Definer()
 
-    # Set dark mode CSS
-    ivr_cleaner.set_dark_mode_css()
+keypress_decoder = Keypress_Decoder()
 
-    tab1, tab2, tab3, tab4 = st.tabs(["IVR_Data_Cleaner", "Questionnaire_Definer", "Keypress_Decoder", "Questionnaire_Keypress_Decoder"])
+questionnaire_keypress_decoder = Questionnaire_Keypress_Decoder()
 
-    with tab1:
-        ivr_cleaner.run()
+tab1, tab2, tab3, tab4 = st.tabs(["IVR_Data_Cleaner", "Questionnaire_Definer", "Keypress_Decoder", "Questionnaire_Keypress_Decoder"])
 
-    with tab2:
-        questionnaire_definer.run2()
+with tab1:
+    ivr_cleaner.run()
 
-    with tab3:
-        keypress_decoder.run3()
+with tab2:
+    questionnaire_definer.run2()
 
-    with tab4:
-        questionnaire_keypress_decoder.run4()
+with tab3:
+    keypress_decoder.run3()
+
+with tab4:
+    questionnaire_keypress_decoder.run4()
